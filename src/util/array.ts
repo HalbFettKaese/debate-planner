b2de661abc8b8ea35e0599b28a6e6c73fb1d7c56
HalbFettKaese <p.wardermann@outlook.de>
2026-05-30 15:48:14 +0200

    Add balancing between experienced and new members

--- src/util/array.ts
+++ src/util/array.ts
@@ -6,7 +6,12 @@
  * @param startIdx The starting index of the relevant subsequence (inclusive)
  * @param endIdx The ending index of the relevant subsequence (exclusive)
  */
-export function shuffleSubsequence<T>(list: T[], startIdx: number, endIdx: number) {
+export function shuffleSubsequence<T>(list: T[], startIdx: number, endIdx: number): void
+export function shuffleSubsequence<T>(list: T[]): void
+export function shuffleSubsequence<T>(list: T[], startIdx?: number, endIdx?: number) {
+    if (startIdx === undefined) startIdx = 0
+    if (endIdx === undefined) endIdx = list.length
+
     const n = endIdx - startIdx // Length of subsequence
     for (var i = n - 1; i >= 1; i--) {
         const j = Math.floor(Math.random() * (i + 1))
@@ -14,4 +19,9 @@ export function shuffleSubsequence<T>(list: T[], startIdx: number, endIdx: numbe
         list[startIdx + i] = list[startIdx + j]
         list[startIdx + j] = temp
     }
+}
+
+export function pickRandom<T>(list: T[]): T | undefined {
+    const idx = Math.floor(Math.random() * list.length)
+    return list[idx]
 }
--- src/util/distribute.ts
+++ src/util/distribute.ts
@@ -1,5 +1,5 @@
 import type { AppStore, RoomDefinition, SlotDefinition } from "@/stores/app";
-import { shuffleSubsequence } from "./array";
+import { pickRandom, shuffleSubsequence } from "./array";
 
 type Draft = Record<string, [string, string]> // Maps from member name to [room, slot]
 
@@ -11,6 +11,7 @@ type Stats = {
 
 type Party = {
     parentName: string,
+    roomId: string,
 } & Stats
 
 type Slot = {
@@ -45,7 +46,7 @@ function getAllParties(appStore: AppStore, wantedRoom?: string) {
                 slotTotal += countTotal
                 slotExperienced += countExperienced
                 const countNew = countTotal - countExperienced
-                slotList.push({ parentName, countTotal, countExperienced, countNew })
+                slotList.push({ parentName, countTotal, countExperienced, countNew, roomId: room.id })
             }
             roomDict[slot.id] = {
                 size: slot.unlimitedSlots ? Infinity : room.slotSize,
@@ -96,7 +97,9 @@ function makeRoomDraft(appStore: AppStore | RoomMap, roomId?: string): Draft {
 }
 
 function makeDraftFromSlots(sourceSlot: Slot, targetSlots: Slot[]) {
-    sourceSlot.parties.sort((a, b) => a.countTotal - b.countTotal)
+    sourceSlot.parties.sort((a, b) =>
+        (a.countTotal - b.countTotal) || (a.roomId == b.roomId ? 0 : a.roomId > b.roomId ? 1 : -1)
+    )
     var lower = 0
     var abort = false
     for (var upper = 1; !abort; upper++) {
@@ -108,7 +111,8 @@ function makeDraftFromSlots(sourceSlot: Slot, targetSlots: Slot[]) {
     }
     // Source parties are now sorted by size (ascending) and otherwise shuffled randomly
     const draft: Draft = {}
-    while (sourceSlot.parties.length > 0) {
+    // Draft the parties that have multiple members
+    while (sourceSlot.parties.length > 0 && sourceSlot.parties[sourceSlot.parties.length - 1].countTotal > 1) {
         const sourceParty = sourceSlot.parties.pop()!
         sourceSlot.countExperienced -= sourceParty.countExperienced
         sourceSlot.countNew -= sourceParty.countNew
@@ -130,10 +134,44 @@ function makeDraftFromSlots(sourceSlot: Slot, targetSlots: Slot[]) {
             moveParty(sourceParty, target, draft)
         }
     }
+    if (sourceSlot.parties.length == 0) {
+        return draft
+    }
+    // Remaining parties each have only a single member
+    const membersExperienced = sourceSlot.parties.filter(party => party.countExperienced > 0)
+    const membersNew = sourceSlot.parties.filter(party => party.countExperienced == 0)
+    const targetsWithoutExperience = targetSlots.filter(slot => slot.definition.free && slot.countExperienced == 0 && slot.countTotal < slot.size && slot.size !== Infinity)
+    while (membersExperienced.length > 0 && targetsWithoutExperience.length > 0) {
+        const movedMember = membersExperienced.pop()!
+        const targetSlot = targetsWithoutExperience.pop()!
+        moveParty(movedMember, targetSlot, draft)
+    }
+    const targetsWithoutNew = targetSlots.filter(slot => slot.definition.free && slot.countNew == 0 && slot.countTotal < slot.size && slot.size !== Infinity)
+    while (membersNew.length > 0 && targetsWithoutNew.length > 0) {
+        const movedMember = membersNew.pop()!
+        const targetSlot = targetsWithoutNew.pop()!
+        moveParty(movedMember, targetSlot, draft)
+    }
+    const remainingMembers = membersExperienced.concat(membersNew)
+    shuffleSubsequence(remainingMembers)
+    while (remainingMembers.length > 0) {
+        const movedMember = remainingMembers.pop()!
+        const limitedTargets = targetSlots.filter(slot => slot.definition.free && slot.size !== Infinity && slot.countTotal < slot.size)
+        if (limitedTargets.length > 0) {
+            const targetSlot = pickRandom(limitedTargets)!
+            moveParty(movedMember, targetSlot, draft)
+            continue
+        }
+        const unlimitedTargets = targetSlots.filter(slot => slot.definition.free && slot.size === Infinity)
+        const targetSlot = pickRandom(unlimitedTargets)
+        if (targetSlot === undefined) break
+        moveParty(movedMember, targetSlot, draft)
+    }
     return draft
 }
 
 function moveParty(party: Party, targetSlot: Slot, draft: Draft) {
+    party.roomId = targetSlot.roomId
     draft[party.parentName] = [targetSlot.roomId, targetSlot.definition.id]
     targetSlot.countExperienced += party.countExperienced
     targetSlot.countNew += party.countNew
